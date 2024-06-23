4585690b452fc248fadfd7232005af9b95bb1942
prestonm44 <preston.mitchell4444@gmail.com>
2024-06-23 14:42:57 -0600

    Basic UI finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Header() {
+
+    return (
+        <header
+            className="App-Header">
+            <article>
+                <h1>Kansas City Chiefs</h1>
+                <p>Here is a list of all the players on the Kansas City football team. As the owner of the franchise, you are responsible for adding or removing any players on the team. You can also edit player information, but we don't recommend that you do that...</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nam cumque molestiae deserunt possimus. Debitis, non minus sunt dolore porro quo, laborum velit sint illo nihil facere tenetur numquam corporis ipsam voluptates! Distinctio dicta qui molestiae, odio accusamus rem inventore delectus consequatur commodi voluptas amet ab est, fugiat, nam et veniam hic eveniet accusantium. Atque tenetur assumenda ipsa nemo placeat ipsam exercitationem neque iste? Alias vero aliquid mollitia at aliquam quidem doloribus est id sint fuga odit corrupti illum unde dolorum ratione similique nulla cumque, praesentium cum cupiditate porro repudiandae incidunt. In, cumque deserunt tempora aliquam fugiat tenetur quaerat qui neque, iure dolor sunt voluptates temporibus dolorem ipsum quidem placeat. Quisquam cumque, ut error omnis asperiores quibusdam id inventore molestiae? Officiis, voluptatibus? Amet, perspiciatis quos voluptatum atque, reprehenderit eos adipisci animi officia neque cumque nihil maiores delectus modi vero necessitatibus sequi quaerat tenetur dolores iste quisquam nulla dicta ratione. Tenetur.</p>
+            </article>
+            <img src='https://kscj.com/wp-content/uploads/2023/06/kansas-city-chiefs-logo-transparent.png' />
+        </header>
+    )
+}
---
+++ src/components/PlayerData.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+
+export default function PlayerData() {
+    const [player, setPlayer] = useState([]);
+
+    useEffect( () => {
+        axios.get('https://api.sportsdata.io/v3/nfl/scores/json/PlayersBasic/KC?key=53ac94d7422e4a65b18e03b16aeb7418').then((res) => {
+            setTimeout(() => {
+                setPlayer(res.data);
+            }, 1000);
+        })
+    })
+
+    const playerList = player.map((play) => 
+    <div className="playerRow">
+        <li key={play.Info}>{play.Name}, {play.Position}, {play.College}, {play.Number} 
+        <div className="buttonColumn">
+        <button className="editButton">Edit</button> <button className="removeButton">Remove</button>
+        </div>
+        </li>
+    </div>);
+    
+    return <ul>{playerList}</ul>;
+
+}
